35a247a53caaa7f42482d9be66c4d723b7c89d6b
jihyoung0103 <jihyoung0103@gmail.com>
2025-03-26 17:02:48 +0900

    FIRST COMMIT

---
+++ backend/src/main/java/com/smu/wordmaster/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.smu.wordmaster.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
