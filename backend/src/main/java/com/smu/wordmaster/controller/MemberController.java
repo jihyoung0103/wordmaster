7e7b4dac9ecd7e1b5a9fa29823e59929d6828b20
jihyoung0103 <jihyoung0103@gmail.com>
2025-05-07 00:45:29 +0900

    로그아웃 기능 추가 세션 유지

--- backend/src/main/java/com/smu/wordmaster/controller/MemberController.java
+++ backend/src/main/java/com/smu/wordmaster/controller/MemberController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/session-login")
+@RequestMapping("/")
 
 public class MemberController {
 
@@ -29,26 +29,34 @@ public class MemberController {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
-        return "index";
+        return "home";
     }
     @GetMapping("/member/login")
     public String loginForm(){
         return "login";
     }
 
-
+    // 로그인
     @PostMapping("/member/login") // session : 로그인 유지
     public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
         MemberDTO loginResult = memberService.login(memberDTO);
+
+        // 로그인 성공 시, 사용자는 Email을 세션으로 부여받게 됨.
         if (loginResult != null) {
-            // login 성공
             session.setAttribute("loginEmail", loginResult.getMemberEmail());
-            return "main";
-        } else {
-            // login 실패
-            return "login";
+            return "main"; // main으로 이동
         }
+        // login 실패 시, 로그인 화면에 머뭄
+        else {return "login";}
     }
+
+    // 로그아웃
+    @GetMapping("/member/logout")
+    public String logout(HttpSession session) {
+        session.invalidate(); // 세션을 무효화하여 로그인 정보를 삭제
+        return "redirect:/";  // 로그인 페이지 URL로 리디렉션
+    }
+
     @GetMapping("/member/")
     public String findAll(Model model) {
         List<MemberDTO> memberDTOList = memberService.findAll();
